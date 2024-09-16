6fd3451e3141089294806ebaa154afd6c0a3fd8e
Michael Tucker <michaeltucker@Michaels-MacBook-Pro.local>
2024-09-17 02:52:36 +1000

    Added reviews

--- ui/src/LandingPage.js
+++ ui/src/LandingPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./styles/LandingPage.css"; // Add custom styles
-// import CurlyArrow from './static/arrow.svg'
+import { AccuracyIcon, CloudIcon, InstantIcon, MultilingualIcon, SocialIcon, WalletIcon } from './icons/icons';
 
 function LandingPage() {
   return (
-    <div className="landing-page min-h-screen  flex flex-col">
+    <div className="landing-page min-h-screen flex flex-col">
         <header className="bg-white shadow-lg p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="flex flex-row h-15 min-w-96 items-center justify-evenly">
@@ -22,7 +22,7 @@ function LandingPage() {
                 </nav>
             </div>
         </header>
-        {/* bg-gradient-to-r from-blue-600 to-teal-500 */}
+
         <main className="hero-pattern min-h-96 flex flex-col content-evenly justify-evenly text-center">
             <div className="text-left p-10 w-90 max-w-4xl mx-4 md:mx-auto">
                 <h2 className="text-4xl font-bold mb-4">
@@ -46,48 +46,122 @@ function LandingPage() {
             </div>
         </div>
 
-        <div className="grid grid-cols-3 gap-4 min-h-96 min-w-96 lg:mx-52 md:mx-40 sm:mx-20 custom-grid">
+        <div className="grid grid-cols-3 gap-4 min-h-96 min-w-96 lg:mx-52 md:mx-40 sm:mx-20 custom-grid mb-10">
             <div className="p-4">
-                <p className="font-bold">Instant Captioning</p>
+                <div>
+                    <InstantIcon className="icon"/>
+                    <p className="font-bold">Instant Captioning</p>
+                </div>
                 <p>Upload your media and in just moments, receive high-quality captions that perfectly match your content. Refine and adjust as needed, all within seconds.</p>
             </div>
             <div className="p-4">
-                <p className="font-bold">Multilingual Support</p>
+                <div>
+                    <MultilingualIcon className="icon"/>
+                    <p className="font-bold">Multilingual Support</p>
+                </div>
                 <p>Expand your reach with our support for 100 languages. Whether you need captions in English, Spanish, Mandarin, or any other language, we've got you covered.</p>
             </div>
             <div className="p-4">
-                <p className="font-bold">No Cloud, No Worries</p>
+                <div>
+                    <CloudIcon className="icon"/>
+                    <p className="font-bold">No Cloud, No Worries</p>
+                </div>
                 <p>Your content stays with you. We generate your captions locally, ensuring complete control over your files without cloud storage.</p>
             </div>
             <div className="p-4">
-                <p className="font-bold">Affordable Pricing</p>
+                <div>
+                    <WalletIcon className="icon"/>
+                    <p className="font-bold">Affordable Pricing</p>
+                </div>
                 <p>Enjoy a cost-effective solution that offers more value for your money. Our competitive pricing ensures you get high-quality captions without breaking the bank.</p>
             </div> 
             <div className="p-4">
-                <p className="font-bold">High Accuracy</p>
+                <div>
+                    <AccuracyIcon className="icon"/>
+                    <p className="font-bold">High Accuracy</p>
+                </div>
                 <p>Our AI-powered tool ensures that your captions are not just fast but also highly accurate, making your content more accessible and engaging.</p>
             </div>
             <div className="p-4">
-                <p className="font-bold">Social Sharing</p>
+                <div>
+                    <SocialIcon className="icon"/>
+                    <p className="font-bold">Social Sharing</p>
+                </div>
                 <p>Easily share your captioned content on social media platforms. Get feedback from your audience and increase your content’s reach effortlessly.</p>
             </div>
         </div>
 
-        <div className="flex flex-col items-center mt-20">
+        <div className="">
+        <div className="flex flex-col items-center mt-10">
             <p className="text-3xl font-bold">Watch How Our App Works</p>
-            <video className="m-10 w-1/2 h-1/2 object-cover shadow-lg" controls>
+            <video className="m-10 w-1/2 h-1/2 object-cover shadow-lg" controls preload="metadata">
                 <source src="/static/demo.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </div>
 
-        <div className="flex items-center justify-center mt-10 mb-20">
+        <div className="flex items-center justify-center mt-10 mb-10">
             <Link to="/generate" className="w-48 bg-blue-600 text-white px-11 py-5 rounded-lg hover:bg-blue-700 transition">
                 Try it out Here
             </Link>
         </div>
+        </div>
+
+        <div className="flex flex-col items-center mt-10 mb-10 ">
+            <p className="text-3xl font-bold">Real feedback from users who love us</p>
+            <div className="m-10 grid grid-cols-2 gap-16 min-h-96 min-w-96 lg:mx-52 md:mx-40 sm:mx-20 custom-grid">
+                <div className="text-center">
+                    <p className="font-bold mb-2">"Super fast and accurate!"</p>
+                    <p className="mb-4">
+                    "I’ve tried a few transcription services, but this one is hands down the best! The turnaround time is insanely quick, and the accuracy is spot on. It saved me hours of manual transcription work!"
+                    </p>
+                    <p className="text-gray-600 italic">— Emily R, Podcast Host</p>
+                </div>
+                
+                <div className="text-center">
+                    <p className="font-bold mb-2">"Perfect for my multilingual projects"</p>
+                    <p className="mb-4">
+                    "The ability to transcribe in various languages with high accuracy is amazing. Highly recommended for global users."
+                    </p>
+                    <p className="text-gray-600 italic">— Luis P, International Business Consultant</p>
+                </div>
+
+                <div className="text-center">
+                    <p className="font-bold mb-2">"Affordable and reliable"</p>
+                    <p className="mb-4">
+                    "This service offers the best value for money. Reliable results every time at an affordable price."
+                    </p>
+                    <p className="text-gray-600 italic">— Jessica L, Freelance Editor</p>
+                </div>
+
+                <div className="text-center">
+                    <p className="font-bold mb-2">"A game-changer for content creators"</p>
+                    <p className="mb-4">
+                    "I love how easy it is to upload my videos and get captions within minutes. It's perfect for my YouTube workflow."
+                    </p>
+                    <p className="text-gray-600 italic">— David M, YouTube Creator</p>
+                </div>
+
+                <div className="text-center">
+                    <p className="font-bold mb-2">"Incredibly easy to use"</p>
+                    <p className="mb-4">
+                    "I was amazed at how simple and intuitive this platform is. I uploaded my video, and within minutes I had perfectly synced captions. It’s a must-have for anyone working with multimedia content."
+                    </p>
+                    <p className="text-gray-600 italic">— Sophie T, Content Marketer</p>
+                </div>
+
+                <div className="text-center">
+                    <p className="font-bold mb-2">"Excellent for accessibility"</p>
+                    <p className="mb-4">
+                    "CaptionGeneratorAI has made my content so much more accessible. The high accuracy and quick turnaround time have been a game-changer for my audience, especially those with hearing impairments."
+                    </p>
+                    <p className="text-gray-600 italic">— Michael B, Online Educator</p>
+                </div>
+            </div>
+        </div>
         
-        <footer className="bg-white p-4 text-center shadow-lg mt-auto">
+        
+        <footer className="bg-white p-4 text-center shadow-lg mt-auto mt-10">
             <p className="text-gray-600">
             &copy; 2024 CaptionGeneratorAI. All rights reserved.
             </p>
---
+++ ui/src/icons/icons.js
@@ -0,0 +1,15 @@
+import { ReactComponent as AccuracyIcon } from './accuracy.svg';
+import { ReactComponent as CloudIcon } from './cloud.svg';
+import { ReactComponent as InstantIcon } from './instant.svg';
+import { ReactComponent as MultilingualIcon } from './multilingual.svg';
+import { ReactComponent as SocialIcon } from './social.svg';
+import { ReactComponent as WalletIcon } from './wallet.svg';
+
+export {
+  AccuracyIcon,
+  CloudIcon,
+  InstantIcon,
+  MultilingualIcon,
+  SocialIcon,
+  WalletIcon,
+};
